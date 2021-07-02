8ce21530bfc3b3967cf3ecb2cee0bb7fd1f19a1f
maheshmojo <33809529+maheshmojo@users.noreply.github.com>
2021-07-03 01:26:24 +0530

    Commit from intelij

--- src/test/java/tech/getarrays/employeemanager/EmployeemanagerApplicationTests.java
+++ src/test/java/tech/getarrays/employeemanager/EmployeemanagerApplicationTests.java
@@ -1,6 +1,6 @@
 package tech.getarrays.employeemanager;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
